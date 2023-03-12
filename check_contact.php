46440de582de20638494d61ed9a2c9f40569ab49
svetlana-kuprina <kupsveta@rambler.ru>
2023-03-12 22:38:32 +0500

    Update check_contact.php

--- check_contact.php
+++ check_contact.php
@@ -8,3 +8,12 @@ session_start();
     $_SESSION['email']=$from;
     $_SESSION['subject']=$subject;
     $_SESSION['message']=$message;
+
+    if (strlen($user_name) <= 1)
+        $error_username = "Введите корректное имя";
+    else if (strlen($from)< 5 || strpos( $from,"@")== false)
+        $error_from="Введите корректный email";
+    else if (strlen($subject)<= 5 )
+        $error_subject="Тема сообщения не менее 5 символов";
+    else if (strlen($message)<= 15 )
+        $error_message="Сообщение не менее 15 символов";
