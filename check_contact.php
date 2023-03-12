61de6b0fca6d3ea5b778728fea4b82fbf2049c08
svetlana-kuprina <kupsveta@rambler.ru>
2023-03-12 21:59:59 +0500

    2
    Урок 19-2

--- check_contact.php
+++ check_contact.php
@@ -1,1 +1,6 @@
 <?php
+session_start();
+    $user_name= htmlspecialchars(trim($_POST['username']));
+    $from= htmlspecialchars(trim($_POST['email']));
+    $subject= htmlspecialchars(trim($_POST['subject']));
+    $message= htmlspecialchars(trim($_POST['message']));
