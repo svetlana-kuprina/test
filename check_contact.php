c1f400cfb217bdbd3e2510568c4b3594c073e0cb
Svetlana <kupsveta@rambler.ru>
2023-03-14 18:02:50 +0500

    3
    урок19-3

--- check_contact.php
+++ check_contact.php
@@ -1,19 +1,40 @@
 <?php
 session_start();
-    $user_name= htmlspecialchars(trim($_POST['username']));
-    $from= htmlspecialchars(trim($_POST['email']));
-    $subject= htmlspecialchars(trim($_POST['subject']));
-    $message= htmlspecialchars(trim($_POST['message']));
+
+function redirect() {
+    header('Location: contact.php');
+    exit();
+}
+    $user_name = htmlspecialchars(trim($_POST['username']));
+    $from = htmlspecialchars(trim($_POST['email']));
+    $subject = htmlspecialchars(trim($_POST['subject']));
+    $message = htmlspecialchars(trim($_POST['message']));
+
     $_SESSION['user_name']=$user_name;
     $_SESSION['email']=$from;
     $_SESSION['subject']=$subject;
     $_SESSION['message']=$message;
 
-    if (strlen($user_name) <= 1)
-        $error_username = "Введите корректное имя";
-    else if (strlen($from)< 5 || strpos( $from,"@")== false)
-        $error_from="Введите корректный email";
-    else if (strlen($subject)<= 5 )
-        $error_subject="Тема сообщения не менее 5 символов";
-    else if (strlen($message)<= 15 )
-        $error_message="Сообщение не менее 15 символов";
+    if (strlen($user_name) <= 1) {
+        $_SESSION['$error_username'] = "Введите корректное имя";
+        redirect();
+    }
+    else if (strlen($from)< 5 || strpos( $from,"@")== false) {
+        $_SESSION['$error_from'] = "Введите корректный email";
+        redirect();
+    }
+    else if (strlen($subject)<= 5 ) {
+        $_SESSION['$error_subject'] = "Тема сообщения не менее 5 символов";
+        redirect();
+    }
+    else if (strlen($message)<= 15 ) {
+        $_SESSION['$error_message'] = "Сообщение не менее 15 символов";
+        redirect();
+    }
+    else {
+       $_SESSION['$error_username'] ="";
+       $_SESSION['$error_from']="";
+       $_SESSION['$error_subject']="";
+       $_SESSION['$error_message']="";
+
+    }
--- contact.php
+++ contact.php
@@ -5,10 +5,14 @@ require "Blocks/header.php";
 ?>
 <h1 class="mt-5"> <?= $title ?> </h1>
 <form action="check_contact.php" method="post">
-    <input type="text" name="username" value="<?=$_SESSION['username']?>" placeholder="Введите имя" class="form-control"> <br>
-    <input type="email" name="email" value="<?=$_SESSION['email']?>" placeholder="Введите email" class="form-control"> <br>
-    <input type="email" name="subject" value="<?=$_SESSION['subject']?>" placeholder="Тема сообщения" class="form-control"> <br>
-    <textarea name="message" placeholder ="Ваше собщение" class="form-control"><?=$_SESSION['message']?> </textarea><br>
+    <input type="text" name="username" value="<?= $_SESSION['username']?>" placeholder="Введите имя" class="form-control">
+    <div class="text-danger"><?= $_SESSION['$error_username']?></div><br>
+    <input type="email" name="email" value="<?=$_SESSION['email']?>" placeholder="Введите email" class="form-control">
+    <div class="text-danger"><?= $_SESSION['$error_from']?></div><br>
+    <input type="text" name="subject" value="<?=$_SESSION['subject']?>" placeholder="Тема сообщения" class="form-control">
+    <div class="text-danger"><?= $_SESSION['$error_subject']?></div><br>
+    <textarea name="message" placeholder ="Ваше собщение" class="form-control"><?=$_SESSION['message']?> </textarea>
+    <div class="text-danger"><?= $_SESSION['$error_message']?></div><br>
     <button type="submit" class="btn btn-success">Отправить </button>
 </form>
 
