5c5a557a866ac1caf0b6bb74be18e417e8f14979
Svetlana <kupsveta@rambler.ru>
2023-03-15 17:59:51 +0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -6,13 +6,13 @@ require "Blocks/header.php";
 <h1 class="mt-5"> <?= $title ?> </h1>
 <form action="check_contact.php" method="post">
     <input type="text" name="username" value="<?= $_SESSION['username']?>" placeholder="Введите имя" class="form-control">
-    <div class="text-danger"><?= $_SESSION['$error_username']?></div><br>
+    <div class="text-danger"><?= print_r($_SESSION['$error_username'])?></div><br>
     <input type="email" name="email" value="<?=$_SESSION['email']?>" placeholder="Введите email" class="form-control">
-    <div class="text-danger"><?= $_SESSION['$error_from']?></div><br>
+    <div class="text-danger"><?= print_r($_SESSION['$error_from'])?></div><br>
     <input type="text" name="subject" value="<?=$_SESSION['subject']?>" placeholder="Тема сообщения" class="form-control">
-    <div class="text-danger"><?= $_SESSION['$error_subject']?></div><br>
+    <div class="text-danger"><?= print_r($_SESSION['$error_subject'])?></div><br>
     <textarea name="message" placeholder ="Ваше собщение" class="form-control"><?=$_SESSION['message']?> </textarea>
-    <div class="text-danger"><?= $_SESSION['$error_message']?></div><br>
+    <div class="text-danger"><?=print_r($_SESSION['$error_message'])?></div><br>
     <button type="submit" class="btn btn-success">Отправить </button>
 </form>
 
