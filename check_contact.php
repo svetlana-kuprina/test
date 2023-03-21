123a51f753d8fed32271552f0c3393030048d07d
svetlana-kuprina <125212069+svetlana-kuprina@users.noreply.github.com>
2023-03-21 21:17:31 +0500

    19

--- check_contact.php
+++ check_contact.php
@@ -15,26 +15,36 @@ function redirect() {
     $_SESSION['subject']=$subject;
     $_SESSION['message']=$message;
 
-    if (strlen($user_name) <= 1) {
-        $_SESSION['$error_username'] = "Введите корректное имя";
-        redirect();
-    }
-    else if (strlen($from)< 5 || strpos( $from,"@")== false) {
-        $_SESSION['$error_from'] = "Введите корректный email";
-        redirect();
-    }
-    else if (strlen($subject)<= 5 ) {
-        $_SESSION['$error_subject'] = "Тема сообщения не менее 5 символов";
-        redirect();
-    }
-    else if (strlen($message)<= 15 ) {
-        $_SESSION['$error_message'] = "Сообщение не менее 15 символов";
-        redirect();
-    }
-    else {
-       $_SESSION['$error_username'] ="";
-       $_SESSION['$error_from']="";
-       $_SESSION['$error_subject']="";
-       $_SESSION['$error_message']="";
+    $_SESSION['$error_username'] = '';
+    $_SESSION['$error_from'] = '';
+    $_SESSION['$error_subject'] = '';
+    $_SESSION['$error_message'] = '';
 
+    if (strlen($user_name) <= 5) {
+        echo("Ошибка имя".'<br>');
+        $_SESSION['$error_username'] = "Введите корректное имя";
+ //       redirect();
     }
+ //   elseif (strlen($from)< 5 || strpos( $from,"@")== false) {
+    if (strlen($from)< 5 || strpos( $from,"@")== false) {
+        echo('Ошибка адрес'.'<br>');
+        $_SESSION['$error_from'] = "Введите корректный email";
+//        redirect();
+    }
+    if (strlen($subject)<= 5 ) {
+        echo('Ошибка тема'.'<br>');
+        $_SESSION['$error_subject'] = "Тема сообщения не менее 5 символов";
+//        redirect();
+    }
+    if (strlen($message)<= 15 ) {
+        echo('Ошибка сообщение'.'<br>');
+        $_SESSION['$error_message'] = "Сообщение не менее 15 символов";
+//        redirect();
+    }
+
+    echo($_SESSION['$error_username'].'!1!'.'<br>');
+    echo($_SESSION['$error_from'].'!2!'.'<br>');
+    echo($_SESSION['$error_subject'].'!3!'.'<br>');
+    echo($_SESSION['$error_message'].'!4!'.'<br>');
+
+    redirect();
